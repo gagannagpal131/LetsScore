94c73b98d5579e4db910f879619fcd141bfc40b4
gagannagpal131 <gagannagpal131@gmail.com>
2020-03-24 20:01:00 -0400

    Files Initialised

---
+++ client.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+
+}
+
