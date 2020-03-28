4246655e67fd80fe769984c30ef7dcef7060c429
gagannagpal131 <gagannagpal131@gmail.com>
2020-03-27 21:22:25 -0400

    Client-Server architecture established

--- client.c
+++ client.c
@@ -10,10 +10,36 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-int main(int argc, char const *argv[])
-{
-	
-	return 0;
+int main(int argc, char const *argv[]) {	
+	int server, portNumber; socklen_t len;
+	struct sockaddr_in servAdd;
+
+ 	if(argc != 3){
+  		printf("Call model:%s <IP> <Port#>\n",argv[0]);
+  		exit(0);
+	}
+
+	if((server = socket(AF_INET, SOCK_STREAM, 0))<0){
+  		fprintf(stderr, "Cannot create socket\n");
+		exit(1); 
+	}
+
+	servAdd.sin_family = AF_INET;
+	sscanf(argv[2], "%d", &portNumber); 
+	servAdd.sin_port = htons((uint16_t)portNumber);
+
+	if(inet_pton(AF_INET,argv[1],&servAdd.sin_addr) < 0){ 
+		fprintf(stderr, " inet_pton() has failed\n"); 
+		exit(2);
+	}
+
+	if(connect(server,(struct sockaddr *)&servAdd,sizeof(servAdd)) < 0){
+ 		fprintf(stderr, "connect() has failed, exiting\n");
+		exit(3);
+	}
+
+	printf("Player Connected to Server!!\n");
+
 
 }
 
--- server.c
+++ server.c
@@ -12,6 +12,33 @@
 
 int main(int argc, char const *argv[])
 {
+	int sd, client1, client2, portNumber;
+	socklen_t len;
+	struct sockaddr_in servAdd;
+
+ 	if(argc != 2){
+    		printf("Call model: %s <Port #>\n", argv[0]);
+    		exit(0);
+	}
+
+	if ((sd=socket(AF_INET,SOCK_STREAM,0))<0){
+   		fprintf(stderr, "Cannot create socket\n");
+		exit(1); 
+		}
+
+	servAdd.sin_family = AF_INET; 
+	servAdd.sin_addr.s_addr = htonl(INADDR_ANY); 
+	sscanf(argv[1], "%d", &portNumber);
+	servAdd.sin_port = htons((uint16_t)portNumber); 
 	
+	bind(sd,(struct sockaddr*)&servAdd,sizeof(servAdd)); 
+	listen(sd, 5);
+
+	client1=accept(sd,(struct sockaddr*)NULL,NULL);
+    	printf("1st Player Connected\n");
+
+	client2=accept(sd,(struct sockaddr*)NULL,NULL);
+	printf("2nd Player Connected\n");    
+
 	return 0;
 }
